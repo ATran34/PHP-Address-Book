9d9bb8edd92482e48355c368f48ab3b4b9ada973
ATran34 <35639389+ATran34@users.noreply.github.com>
2019-04-11 00:07:09 -0500

    Add files via upload

--- Table.php
+++ Table.php
@@ -27,6 +27,10 @@ include_once 'connection.php';
       <input type ="text" name ="phone" placeholder="Phone Number">
       <br>
       <input type = "submit" name = "submit">
+      <input type = "submit" name = "lastorder" value= "Last Name Ascending Order">
+      <input type = "submit" name = "lastorderD" value= "Last Name Descending Order">
+      <input type = "submit" name = "cityorder" value= "List by City">
+      <input type = "submit" name = "stateorder" value= "List by State">
     </form>
 
     <?php
@@ -41,13 +45,18 @@ include_once 'connection.php';
     $email = ($_POST['email']);
     $phone = ($_POST['phone']);
 
+    //Input constraints
     if($_POST['first'] == "" || $_POST['last'] == ""){
-      echo "Please enter your First and Last Name <br>";
+      echo "Information was not added.<br>Please enter your First and Last Name <br>";
     }
     else if($_POST['address'] != "" && ($_POST['city'] == "" ||
      $_POST['state'] == "" || $_POST['zip'] == "")) {
-       echo "Please enter City, State, and Zip with your Street Address";
+       echo "Information was not added.<br>Please enter City, State, and Zip with your Street Address<br>";
      }
+    /* else if(count($_POST['state']) != 2){
+       echo "Information was not added.<br>Please enter State as 2 intials";
+     }*/
+     //if requirements are met, insert data into SQL server
     else{
     $sql = "INSERT INTO people1 (firstName, lastName, streetAddress, City,
       State, Zip, Email, Phone)  VALUES ('$first', '$last', '$address',
@@ -57,28 +66,54 @@ include_once 'connection.php';
       }
 
      ?>
-     <table>
-       <tr>
-         <th>First Name </th>
-         <th>Last Name </th>
-         <th>Street Address </th>
-         <th>City </th>
-         <th>State </th>
-         <th>Zip </th>
-         <th>Email </th>
-         <th>Phone Number </th>
-         <th>Date of Last Contact </th>
-         <th>Creation date/time </th>
 
       <?php
-     $table = "SELECT firstName, lastName, streetAddress, City, State, Zip,
-     Email, Phone, lastContactDate, creationDate from people1";
-     $result = $conn->query($table);
 
-     if($result-> num_rows > 0){
+    //SELECT statements for table
+     $table = "SELECT firstName, lastName, streetAddress, City, State, Zip,
+     Email, Phone, lastContactDate, creationDate FROM people1";
+
+     $tablelast = "SELECT lastName, firstName, streetAddress, City, State, Zip,
+     Email, Phone, lastContactDate, creationDate FROM people1
+     ORDER BY lastName ASC";
+
+     $tablelastD = "SELECT lastName, firstName, streetAddress, City, State, Zip,
+     Email, Phone, lastContactDate, creationDate FROM people1
+     ORDER BY lastName DESC";
+
+     $tablecity = "SELECT lastName, firstName, streetAddress, City, State, Zip,
+     Email, Phone, lastContactDate, creationDate FROM people1
+     ORDER BY City ASC";
+
+     $tablestate = "SELECT lastName, firstName, streetAddress, City, State, Zip,
+     Email, Phone, lastContactDate, creationDate FROM people1
+     ORDER BY State ASC";
+
+     //SELECT implementation to server
+     $result = $conn->query($table);
+     $resultlast = $conn->query($tablelast);
+     $resultlastD = $conn->query($tablelastD);
+     $resultcity = $conn->query($tablecity);
+     $resultstate = $conn->query($tablestate);
+
+     //Default table layout
+     if(isset($_POST['submit'])){
+       if($result-> num_rows > 0){
+         echo "<table>
+          <tr>
+           <th>First Name </th>
+           <th>Last Name </th>
+           <th>Street Address </th>
+           <th>City </th>
+           <th>State </th>
+           <th>Zip </th>
+           <th>Email </th>
+           <th>Phone Number </th>
+           <th>Date of Last Contact </th>
+           <th>Creation date/time </th>";
        while($row = $result-> fetch_assoc()){
          echo
-         "<tr><td>" . $row["firstName"] . "</td><td>" . $row["lastName"] .
+         "<tr><td>" .  $row["firstName"] . "</td><td>" . $row["lastName"] .
          "</td><td>" . $row["streetAddress"] . "</td><td>" . $row["City"] .
          "</td><td>" . $row["State"] . "</td><td>" . $row["Zip"] .
          "</td><td>" . $row["Email"] . "</td><td>" . $row["Phone"] .
@@ -88,7 +123,126 @@ include_once 'connection.php';
        echo "</table>";
      }else{
        echo "Table is currently empty<br>";
+         }
+       }
+
+   //List by LastName
+     if(isset($_POST['lastorder'])){
+      if($resultlast-> num_rows > 0){
+       echo "<table>
+        <tr>
+         <th>Last Name </th>
+         <th>First Name </th>
+         <th>Street Address </th>
+         <th>City </th>
+         <th>State </th>
+         <th>Zip </th>
+         <th>Email </th>
+         <th>Phone Number </th>
+         <th>Date of Last Contact </th>
+         <th>Creation date/time </th>";
+       while($row = $resultlast-> fetch_assoc()){
+       echo
+       "<tr><td>" .  $row["lastName"] . "</td><td>" . $row["firstName"] .
+       "</td><td>" . $row["streetAddress"] . "</td><td>" . $row["City"] .
+       "</td><td>" . $row["State"] . "</td><td>" . $row["Zip"] .
+       "</td><td>" . $row["Email"] . "</td><td>" . $row["Phone"] .
+       "</td><td>" . $row["lastContactDate"] . "</td><td>" . $row["creationDate"] .
+       "</td></tr>";
      }
+     echo "</table>";
+   }else{
+     echo "Table is currently empty<br>";
+       }
+     }
+     //List by LastName Descending
+     if(isset($_POST['lastorderD'])){
+       if($resultlastD-> num_rows > 0){
+         echo "<table>
+          <tr>
+           <th>Last Name </th>
+           <th>First Name </th>
+           <th>Street Address </th>
+           <th>City </th>
+           <th>State </th>
+           <th>Zip </th>
+           <th>Email </th>
+           <th>Phone Number </th>
+           <th>Date of Last Contact </th>
+           <th>Creation date/time </th>";
+       while($row = $resultlastD-> fetch_assoc()){
+         echo
+         "<tr><td>" .  $row["lastName"] . "</td><td>" . $row["firstName"] .
+         "</td><td>" . $row["streetAddress"] . "</td><td>" . $row["City"] .
+         "</td><td>" . $row["State"] . "</td><td>" . $row["Zip"] .
+         "</td><td>" . $row["Email"] . "</td><td>" . $row["Phone"] .
+         "</td><td>" . $row["lastContactDate"] . "</td><td>" . $row["creationDate"] .
+         "</td></tr>";
+       }
+       echo "</table>";
+     }else{
+       echo "Table is currently empty<br>";
+         }
+       }
+    //List by City
+        if(isset($_POST['cityorder'])){
+         if($resultcity-> num_rows > 0){
+           echo "<table>
+            <tr>
+             <th>City </th>
+             <th>First Name </th>
+             <th>Last Name </th>
+             <th>Street Address </th>
+             <th>State </th>
+             <th>Zip </th>
+             <th>Email </th>
+             <th>Phone Number </th>
+             <th>Date of Last Contact </th>
+             <th>Creation date/time </th>";
+         while($row = $resultcity-> fetch_assoc()){
+           echo
+           "<tr><td>" .  $row["City"] . "</td><td>" . $row["firstName"] .
+           "</td><td>" . $row["lastName"] . "</td><td>" . $row["streetAddress"] .
+           "</td><td>" . $row["State"] . "</td><td>" . $row["Zip"] .
+           "</td><td>" . $row["Email"] . "</td><td>" . $row["Phone"] .
+           "</td><td>" . $row["lastContactDate"] . "</td><td>" . $row["creationDate"] .
+           "</td></tr>";
+         }
+         echo "</table>";
+       }else{
+         echo "Table is currently empty<br>";
+           }
+         }
+
+      //List by State
+      if(isset($_POST['stateorder'])){
+          if($resultstate-> num_rows > 0){
+            echo "<table>
+             <tr>
+              <th>State </th>
+              <th>First Name </th>
+              <th>Last Name </th>
+              <th>Street Address </th>
+              <th>City </th>
+              <th>Zip </th>
+              <th>Email </th>
+              <th>Phone Number </th>
+              <th>Date of Last Contact </th>
+              <th>Creation date/time </th>";
+          while($row = $resultstate-> fetch_assoc()){
+            echo
+            "<tr><td>" .  $row["State"] . "</td><td>" . $row["firstName"] .
+            "</td><td>" . $row["lastName"] . "</td><td>" . $row["streetAddress"] .
+            "</td><td>" . $row["City"] . "</td><td>" . $row["Zip"] .
+            "</td><td>" . $row["Email"] . "</td><td>" . $row["Phone"] .
+            "</td><td>" . $row["lastContactDate"] . "</td><td>" . $row["creationDate"] .
+            "</td></tr>";
+          }
+          echo "</table>";
+        }else{
+          echo "Table is currently empty<br>";
+            }
+          }
      ?>
       </tr>
    </table>
